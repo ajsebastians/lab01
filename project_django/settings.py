afb921b6b0c894c6d4a00b44db35af81617e867a
Bastian <bastian@Bastians-MacBook-Pro-3.local>
2022-09-08 13:33:38 +0700

    finishing

--- project_django/settings.py
+++ project_django/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
-import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'example_app',
+    'wishlist',
 ]
 
 MIDDLEWARE = [
